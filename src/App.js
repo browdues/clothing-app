fe01146cfbf8b7e61607e836bb62ebcfbd43fe01
Nicholas Browdues <browdues@gmail.com>
2020-03-18 20:21:10 -0600

    :zap: Added header component with links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={Homepage}/>
-        <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
